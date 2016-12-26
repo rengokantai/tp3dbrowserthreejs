a68c1041a6b2b40470a3d3a986b1af3dc43e8f3b
Hernan Y.Ke <yuriqiao@gmail.com>
2016-12-26 12:40:46 -0500

    createCamera createRenderer

--- L01/script.js
+++ L01/script.js
@@ -1,11 +1,22 @@
 var scene = new THREE.Scene(),
-light = new THREE.PointLight(0xffffff,1),
-camera = new THREE.PerspectiveCamera(35,window.innerWidth/window.innerHeight,1,1000),
-renderer = new THREE.WebGLRenderer()
+light = new THREE.PointLight(0xffffff,1)
 
-camera.position.z=10;
-light.position.set(0,0,50);
-renderer.setSize(window.innerWidth,window.innerHeight);
-scene.add(light);
+var createCamera = function(){
+	var camera = new THREE.PerspectiveCamera(35,window.innerWidth/window.innerHeight,1,1000);
+	camera.position.z=10;
+	return camera;
+}
+
+var createRenderer = function(){
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth,window.innerHeight);
 
 document.querySelector('#container').appendChild(renderer.domElement);
+	return renderer;
+}
+var camera = createCamera();
+var renderer = createRenderer();
+light.position.set(0,0,50);
+scene.add(light);
+
+renderer.render(scene,camera);
